163cde219536776257987ff7b3c1bbf1d355b713
ScarfaceHBC <p.jipp87@gamil.com>
2022-09-15 19:02:23 +0200

    ...

---
+++ js/script.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", () => {
+  document.getElementById("button").addEventListener("click", () => {
+    alert("Diese Seite ist nur ein HTML und CSS Showcase");
+  });
+});
